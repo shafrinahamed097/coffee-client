c50c4efb0657c57d40bcb93e26da34414814004a
shafrinahamed097 <shafrinahamedsa@gmail.com>
2025-08-07 15:51:55 +0600

    Create a form

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,32 +1,42 @@
 const AddCoffee = () => {
   return (
-    <div>
-      <h2>Add A Coffee</h2>
+    <div className="bg-[#f4f3f0f7] p-24">
+      <h2 className="text-black text-3xl font-extrabold">Add A Coffee</h2>
       <form>
-        <div className="flex gap-4">
-          <div className="form-control">
-            <labal className="label">
-              <span className="label-text">Coffee Name </span>
-            </labal>
-            <br />
-            <div className="join">
-              <input className="input join-item" placeholder="Coffee Name" />
-              
-            </div>
-          </div>
-            <div className="form-control">
-            <labal className="label">
-              <span className="label-text">Available Quantity </span>
-            </labal>
-            <br />
-            <div className="join">
-              <input className="input join-item" placeholder="Available Quantity" />
-              
-            </div>
-          </div>
-        </div>
-        
-      </form>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label text-white">Name</label>
+                        <input type="text" name='name' className="input w-full" placeholder="Coffee Name" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label text-white">Quantity</label>
+                        <input type="text" name='quantity' className="input w-full" placeholder="Quantity Name" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label text-white">Supplier</label>
+                        <input type="text" name='supplier' className="input w-full" placeholder="Supplier Name" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label text-white">Taste</label>
+                        <input type="text" name='taste' className="input w-full" placeholder="Taste Name" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label text-white">Price</label>
+                        <input type="text" name='price' className="input w-full" placeholder="Price per Cup" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label text-white">Details</label>
+                        <input type="text" name='details' className="input w-full" placeholder="Details Name" />
+                    </fieldset>
+                </div>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border my-6 p-4">
+                    <label className="label text-white">Photo</label>
+                    <input type="text" name='photo' className="input w-full" placeholder="Photo URL" />
+                </fieldset>
+
+                <input type="submit" className='btn w-full' value="Add Coffee" />
+            </form>
+      
     </div>
   );
 };
